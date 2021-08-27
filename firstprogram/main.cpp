c36f2633d3885b2ec862387b4c9b23fa5c6df8a4
Nadina Amsler <nadina.amsler@icloud.com>
2021-08-27 16:00:35 +0200

    header

--- firstprogram/main.cpp
+++ firstprogram/main.cpp
@@ -1,5 +1,10 @@
 // my first C++ program
 
+/*
+ * @author Nadina Amsler(shirin197)
+ * @date   27.08.2021
+ */
+
 #include <iostream> // include the basic input | output libary
 using namespace std; // declare to get access to the functionality
 
@@ -22,7 +27,21 @@ int main (){
     }
     while (x > i);
 
+
+    // Benutzereingabe
+    unsigned ii = 1;
+    unsigned benutzer = 0;
+
+    cout <<"Bitte hebe ein Zahl ein" << endl;
+    cin >> benutzer;
+
+    do{
+        cout << ii << :: endl;
+        ii++;
+    }while(ii <= benutzer);
+
     mysecondmain myObj;
     myObj.mysecondMeth();
+
     return 0;
 }
