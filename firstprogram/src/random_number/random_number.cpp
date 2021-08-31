fbc46b0fce1f5751b6a945f7f5fee17a974c6988
Nadina Amsler <nadina.amsler@icloud.com>
2021-08-31 08:55:42 +0200

    correctioname

--- firstprogram/src/random_number/random_number.cpp
+++ firstprogram/src/random_number/random_number.cpp
@@ -1,10 +1,17 @@
 //
-// Created by Nadina Amslerm (shirin197)  on 31.08.2021.
+// Created by Nadina Amsler (shirin197)  on 31.08.2021.
+// main topic -> rand function ::
 //
 
 #include <stdio.h>      /* printf, scanf, puts, NULL */
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
+
+/*
+ * v1 = rand() % 100;         // v1 in the range 0 to 99
+ * v2 = rand() % 100 + 1;     // v2 in the range 1 to 100
+ * v3 = rand() % 30 + 1985;   // v3 in the range 1985-2014
+ * */
 
 int main(){
     int iSecret, iGuess;
