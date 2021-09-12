67a4247ddbc033000c23f025019d288f9382ee1a
Nadina Amsler <nadina.amsler@icloud.com>
2021-09-12 22:38:17 +0200

    10.09

--- firstprogram/src/method/method.cpp
+++ firstprogram/src/method/method.cpp
@@ -8,59 +8,59 @@
  * at the moment, when you choose a method and then choose another the balance is the one from the start !!
  */
 #include "iostream"
+
 using namespace std;
 
 // Global variables
 
-    // different account balance variables
-    int am_m_01;
-    int am_m_02;
-    float accountbalance;
+// different account balance variables
+float am_m_01;
+float am_m_02;
+float accountbalance;
 
-    // method exit to end the programm
-    void exit(){
-        cout << "exit";
-    };
+// method exit to end the program
+void exit() {
+    cout << "exit";
+};
 
-    // method to add money to the current account balance
-    void add(){
+// method to add money to the current account balance
+void add() {
 
-            cout << "Add money\n";
-            cout << "---------\n";
-            cout << "How much money do you wanna add: ";
-            cin >> am_m_01;
-            int add_acb = accountbalance + am_m_01;
+    cout << "Add money\n";
+    cout << "---------\n";
+    cout << "How much money do you wanna add: ";
+    cin >> am_m_01;
+    int draw_acb01 = accountbalance + am_m_01;
+    cout << "Your new account balance is: " << draw_acb01 << ".Fr\n";
+}
 
-            cout << "Your new account balance is: " << add_acb << ".Fr\n";
+// method to draw money from the current account balance
+void draw() {
+    cout << "draw money\n";
+    cout << "---------\n";
+    cout << "How much money do you wanna draw: ";
+    cin >> am_m_02;
 
-        }
-    // method to draw money from the current account balance
-    void draw(){
-        cout << "draw money\n";
-        cout << "---------\n";
-        cout << "How much money do you wanna draw: ";
-        cin >> am_m_02;
+    int draw_acb = accountbalance - am_m_02;
 
-        int draw_acb = accountbalance - am_m_02;
-
-        if (draw_acb < 0){
-            cout << "You dont have enough money!";
-        } else {
-            cout << "Your new account balance is: " << draw_acb << ".Fr\n";
-        }
-
+    if (draw_acb < 0) {
+        cout << "You dont have enough money!";
+    } else {
+        cout << "Your new account balance is: " << draw_acb << ".Fr\n";
     }
 
-int main(){
+}
+
+int main() {
     // variable for the input
     int input;
 
-        cout << "How much money do you have: ";
-        cin >> accountbalance;
-        cout << "Your current account balance: " << accountbalance << ".Fr \n";
+    cout << "How much money do you have: ";
+    cin >> accountbalance;
+    cout << "Your current account balance: " << accountbalance << ".Fr \n";
 
-        // do while loop
-        do {
+    // do while loop
+    do {
         cout << "Choose an option \n";
         cout << "--------------- \n";
         cout << "When you want to add money press '1' \n";
@@ -70,27 +70,27 @@ int main(){
         cin >> input;
 
         // switch case to choose the method
-        switch ( input ){
+        switch (input) {
             case 1:
                 add();
                 break;
 
-                case 2:
-                    draw();
-                    break;
+            case 2:
+                draw();
+                break;
 
-                    case 3:
-                        exit();
-                        break;
+            case 3:
+                exit();
+                break;
 
             case 4: {
-                if(input < 3){
+                if (input < 3) {
                     cout << "This not an option choose one from 1-3!\n";
                     break;
                 }
             }
         }
-    }while(input != 3);
+    } while (input != 3);
 
     return 0;
 }
